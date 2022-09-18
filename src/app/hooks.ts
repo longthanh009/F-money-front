0c555d03888538dd3c38cf68807f2bab255eb445
khuatlong0902 <khuatlong0902@gmail.com>
2022-09-18 12:55:06 +0700

    create project f-money

---
+++ src/app/hooks.ts
@@ -0,0 +1,6 @@
+import { useSelector, TypedUseSelectorHook, useDispatch} from "react-redux";
+import type { RootState,AppDispatch } from "./store";
+
+
+export const useAppDispatch = () =>useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
