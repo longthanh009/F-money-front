93e2fc15812f9afbd854aa391fa357cc2dd868db
khuatlong0902 <khuatlong0902@gmail.com>
2022-10-03 22:34:10 +0700

    FM-17 UI-SigninPage

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
-
+import 'antd/dist/antd.css';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
