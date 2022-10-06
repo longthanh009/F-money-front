426f007359140025a24128062e3845b935f801ea
daibede <xuandai300092@gmail.com>
2022-10-06 23:11:49 +0700

    FM-23-ui-lender-danh-sach-khach-hang

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import CustomerList from '../pages/Lender/CustomerList'
 import Dashboard from '../pages/Lender/Dashboard'
 import SiginPage from '../pages/Signin'
 import SignupPage from '../pages/Signup'
@@ -11,6 +12,7 @@ const RoutePage = () => {
                 <Route path='/signin' element={<SiginPage/>}></Route>
                 <Route path='/register' element={<SignupPage/>}></Route>
                 <Route path="/" element={<Dashboard />} />
+                <Route path="/lender/customer/list" element={<CustomerList />} />
             </Routes>
         </div>
     )
