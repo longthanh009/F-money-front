8b858472896e29c52d7c0bfd6890b89ca47f718f
manhkkkk <79675048+manhkkkk@users.noreply.github.com>
2022-10-07 23:47:45 +0700

    creat tinchap

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import TitleTop from './components/titleTop/TitleTop'
+import Unsecured from './pages/admin/Unsecured'
 import RoutePage from './Routes'
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <RoutePage></RoutePage>
-    </>
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<TitleTop />}>
+          <Route index element={<Unsecured />} />
+        </Route>
+      </Routes>
+    </div>
   )
 }
 
