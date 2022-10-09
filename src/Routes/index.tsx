ec4a1319a2483139977257d11c6b4a972a69d067
Tramnvph14967 <tramnvph14967@fpt.edu.vn>
2022-10-09 13:47:43 +0700

    Update

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -7,8 +7,9 @@ import Dashboard from '../pages/Lender/Dashboard'
 import SiginPage from '../pages/Signin'
 import SignupPage from '../pages/Signup'
 import AdminContractPage from '../pages/AdminContract'
-import Website_Layout from '../pages/Layout/Website_Layout'
+import Website_Layout from '../layouts/LayoutWebsite'
 import HomePage from '../pages/HomePage'
+import SuportPage from '../pages/SuportPage'
 import DebitCustomers from '../pages/Lender/DebitCustomers'
 const RoutePage = () => {
     return (
@@ -17,11 +18,16 @@ const RoutePage = () => {
                 <Route path="/" element={<Dashboard />} />
                 <Route path='/client' element={<Website_Layout />}>
                     <Route index element={<HomePage />} />
+                    <Route path='suport' element={<SuportPage />} />
                 </Route>
+
+
                 <Route path="/lender/customer/list" element={<CustomerList />} />
                 <Route path="/lender/customer/debitCustomer" element={<DebitCustomers />} />
                 <Route path='/signin' element={<SiginPage />}></Route>
                 <Route path='/register' element={<SignupPage />}></Route>
+
+
                 <Route path="/" element={<LayoutDashboard />} >
                     <Route index element={<Dashboard />} />
                 </Route>
---
+++ src/pages/SuportPage/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SuportPage = () => {
+  return (
+    <div>SuportPage</div>
+  )
+}
+
+export default SuportPage
