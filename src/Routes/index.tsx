f34e31e3bfbd2ae459aa1420faea8596dd8728a0
khuatlong0902 <khuatlong0902@gmail.com>
2022-10-09 11:20:34 +0700

    FM-31-UI-Quan-ly-hop-dong

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -7,17 +7,21 @@ import AdminLender from '../pages/AdminLender'
 import Dashboard from '../pages/Lender/Dashboard'
 import SiginPage from '../pages/Signin'
 import SignupPage from '../pages/Signup'
+import AdminContractPage from '../pages/AdminContract'
 const RoutePage = () => {
     return (
         <div>
             <Routes>
-                <Route path="/" element={<Dashboard />} />
                 <Route path="/lender/customer/list" element={<CustomerList />} />
                 <Route path='/signin' element={<SiginPage />}></Route>
                 <Route path='/register' element={<SignupPage />}></Route>
                 <Route path="/" element={<LayoutDashboard />} >
                     <Route index element={<Dashboard />} />
-                    <Route path='admin/customer' element={<AdminLender />} />
+                </Route>
+                <Route path="/admin" element={<LayoutDashboard />} >
+                    <Route index element={<Dashboard />} />
+                    <Route path='customer' element={<AdminLender />} />
+                    <Route path='contract' element={<AdminContractPage />} />
                 </Route>
             </Routes>
         </div>
