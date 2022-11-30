3fb2f86d28a762fcc8c877dc028d07b688ea9580
tramnvph14967 <tramnvph14967@gmail.com>
2022-11-30 21:31:16 +0700

    Feature -Tạo yêu cầu hỗ trợ

--- src/pages/SuportPage/index.tsx
+++ src/pages/SuportPage/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Banner from '../HomePage/Banner'
 import Suport_Page from './Suport_Page';
+
+
 const SuportPage = () => {
   return (
     <>
       {/**Banner */}
       <Banner />
       {/**Suport */}
-     <Suport_Page />
+      <Suport_Page />
 
     </>
   )
