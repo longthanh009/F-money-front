789d1cab66fdeb25772c4f566a59913a5ace0352
tramnvph14967 <tramnvph14967@gmail.com>
2023-01-11 14:48:30 +0700

    create_UI-QuenMatKhau

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -37,7 +37,7 @@ import AdminSupport from "../pages/AdminSupport/AdminSupport";
 import MenuLoan from "../pages/Lender/Menuloan";
 import Rules from "../pages/Rules";
 import CustomConta from "../pages/Lender/CustomConta";
-
+import ForgetPage from "../pages/ForgetPage/forgetPage";
 const RoutePage = () => {
   return (
     <div>
@@ -48,6 +48,7 @@ const RoutePage = () => {
           {/**Thông tin tài khoản */}
           <Route path="password/:id" element={<Passwword />} />{" "}
           {/**Đổi mật khẩu */}
+          
           <Route path="suport" element={<SuportPage />} /> {/**Hỗ trợ */}
           <Route path="contractPage" element={<ContractPage />} />{" "}
           <Route path="rule" element={<Rules />} /> {/**Yêu cầu hợp đồng */}
@@ -91,9 +92,7 @@ const RoutePage = () => {
           {/**Yêu cầu vay tiền*/}
         </Route>
         <Route path="/signin" element={<SiginPage />}></Route> {/**Đăng nhập */}
-        <Route path="/register" element={<SignupPage />}>
-          {" "}
-        </Route>{" "}
+        <Route path="/register" element={<SignupPage />}>{" "}</Route>{" "}
         {/**Đăng ký */}
         <Route
           path="/admin"
---
+++ src/pages/ForgetPage/ForgetPage.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Form, Input, Button, Space } from 'antd';
+import type { FormItemProps } from 'antd';
+
+const MyFormItemContext = React.createContext<(string | number)[]>([]);
+
+interface MyFormItemGroupProps {
+  prefix: string | number | (string | number)[];
+  children: React.ReactNode;
+}
+
+function toArr(str: string | number | (string | number)[]): (string | number)[] {
+  return Array.isArray(str) ? str : [str];
+}
+
+const MyFormItemGroup = ({ prefix, children }: MyFormItemGroupProps) => {
+  const prefixPath = React.useContext(MyFormItemContext);
+  const concatPath = React.useMemo(() => [...prefixPath, ...toArr(prefix)], [prefixPath, prefix]);
+
+  return <MyFormItemContext.Provider value={concatPath}>{children}</MyFormItemContext.Provider>;
+};
+
+const MyFormItem = ({ name, ...props }: FormItemProps) => {
+  const prefixPath = React.useContext(MyFormItemContext);
+  const concatName = name !== undefined ? [...prefixPath, ...toArr(name)] : undefined;
+
+  return <Form.Item name={concatName} {...props} />;
+};
+
+const ForgetPage: React.FC = () => {
+  const onFinish = (value: object) => {
+    console.log(value);
+  };
+
+  return (
+    <Form name="form_item_path" layout="vertical" onFinish={onFinish}>
+      <div className='p-4'>
+        <h1 className='text-2xl font-bold'>Đặt lại mật khẩu</h1> <br />
+
+        <Form.Item name="email" label="Email (Vui lòng nhập email đăng ký tài khoản)"
+          rules={[{ required: true, message: 'Vui lòng nhập email' },
+          { type: 'email', message: 'Vui lòng nhập đúng định dạng email' }]}>
+          <Input />
+        </Form.Item>
+      </div>
+      <Button className='p-4' type="primary" htmlType="submit">
+        Xác nhận
+      </Button>
+    </Form>
+  );
+};
+
+export default ForgetPage;
+
